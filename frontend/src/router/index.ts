aa3e52381109420e0c6f2bbf858db4f96d643ee5
Anup Sam Abraham <anupsabraham@gmail.com>
2026-06-18 08:25:32 +0530

    impr: added page title

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import TransactionsPage from '@/pages/TransactionsPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomePage },
-    { path: '/transactions', component: TransactionsPage },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: HomePage, meta: {title: "Dashboard"}},
+        {path: '/transactions', component: TransactionsPage, meta: {title: "Transactions"}},
+    ],
+})
+
+router.beforeEach((to) => {
+    document.title = to.meta.title ? `${to.meta.title} - Peanut Trail` : 'Peanut Trail'
 })
 
 export default router
